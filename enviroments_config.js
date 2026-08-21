ae56e9891b0c88547cf8c84e74f032dadf556909
Eduardo Rosa <edu.wd2014@gmail.com>
2026-08-20 22:42:31 -0300

    ✨ feat: ajustes pages

---
+++ enviroments_config.js
@@ -0,0 +1,7 @@
+const SUPABASE_CONFIG = {
+    url: 'https://SEU-PROJETO.supabase.co',
+    anonKey: 'SUA-CHAVE-ANON-PUBLICA',
+    bucket: 'materiais',
+    redirectUrl: 'https://seusite.github.io/login.html',
+    environment: 'production'
+};
